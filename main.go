096e93782e929057ae7747f0f08796b95f29b7e4
Reckhou <reckhou@me.com>
2013-06-05 19:37:30 +0800

    完成模拟战斗功能，加入统计

--- main.go
+++ main.go
@@ -15,33 +15,70 @@ const (
   CARD_MIN = 8
   CARD_MAX = 39 // 3-10
   DECK_SIZE = CARD_MAX - CARD_MIN + 1
-  HAND_SIZE = 10
-  MAX_DRAW_AMOUNT = 4
+  HAND_SIZE = 15
+  MAX_DRAW_AMOUNT = 2
+  TOTAL_BATTLES = 10000
 )
 
 var gDeck []int
 var gHand []int
+var gDamage int
 
 func main() {
   round := 1
-  for {
+  battle := 1
+  gDamage = 0
+  totalDamage := 0
+  maxDamage := 0
+  minDamage := 10000
+  totalRounds := 0
+  maxRound := 0
+  minRound := 100
+  /*for {
     fmt.Printf("====== 第%d轮 ======\n", round)
     initDeck()
     initHand()
     attack()
+    return
     round++
-  }
+  }*/
   initDeck()
   printCardSet(gDeck, "Deck")
   initHand()
 
+  for battle <= TOTAL_BATTLES {
+    initDeck()
+    printCardSet(gDeck, "Deck")
+    initHand()
+    gDamage = 0
+    round = 1
+    for len(gDeck) !=0 && gDamage < 1500 {
+      fmt.Printf("====== 第%d场第%d轮 ======\n", battle, round)
+      drawFromDeck(gHand, MAX_DRAW_AMOUNT)
+      attack()
+      round++
+    }
+    fmt.Printf("====== 本场战斗总伤害:%d ======\n", gDamage)
+    totalDamage += gDamage
+    if gDamage > maxDamage {
+      maxDamage = gDamage
+    }
+    if gDamage < minDamage {
+      minDamage = gDamage
+    }
 
-  for len(gDeck) !=0 {
-    fmt.Printf("====== 第%d轮 ======\n", round)
-    drawFromDeck(gHand, MAX_DRAW_AMOUNT)
-    attack()
-    round++
+    if round > maxRound {
+      maxRound = round
+    }
+    if round < minRound {
+      minRound = round
+    }
+
+    battle++
+    totalRounds += round
   }
+
+  fmt.Printf("====== 每场战斗平均伤害:%d MAX:%d MIN:%d ======\n====== 每场战斗平均经过回合:%d MAX:%d MIN:%d ======\n", totalDamage/TOTAL_BATTLES, maxDamage, minDamage, totalRounds/TOTAL_BATTLES, maxRound, minRound)
 }
 
 func initDeck() () {
@@ -79,6 +116,7 @@ func drawFromDeck(hand []int, amount int) {
 }
 
 func printCardSet(card_set []int, desc string) {
+  return
   fmt.Printf("\n%s(%d left):[", desc, len(card_set))
   for i, num := range card_set {
     if i%4 == 0 {
@@ -110,7 +148,7 @@ func shuffleDeck_Fisher_Yates(deck []int) {
   }
   r := rand.New(rand.NewSource(time.Now().UnixNano()))
   for ;card_cnt !=0; card_cnt-- {
-    j := r.Int() % (card_cnt+1);
+    j := r.Int() % (card_cnt);
     temp = deck[card_cnt-1];
     deck[card_cnt-1] = deck[j];
     deck[j] = temp;
@@ -135,11 +173,10 @@ func attack() {
     if num%4 == 0 {
       if i+4 <= len(tmp) {
         if tmp[i+1] == num+1 && tmp[i+2] == num+2 && tmp[i+3] == num+3 {
-          fmt.Printf("i:%d\n", i)
           bomb = append(bomb, tmp[i:i+4]...)
           tmp = append(tmp[:i], tmp[i+4:]...)
-          printCardSet(bomb, "bomb")
-          printCardSet(tmp, "tmp")
+          //printCardSet(bomb, "bomb")
+          //printCardSet(tmp, "tmp")
           i = -1
           continue
         }
@@ -151,11 +188,10 @@ func attack() {
     // find triple
     if i+3 <= len(tmp) {
       if tmp[i+1]/4 == tmp[i]/4 && tmp[i+2]/4 == tmp[i]/4 {
-        fmt.Printf("tmp[%d]: %d\n", i, tmp[i])
         triple = append(triple, tmp[i:i+3]...)
         tmp = append(tmp[:i], tmp[i+3:]...)
-        printCardSet(triple, "triple")
-        printCardSet(tmp, "tmp")
+        //printCardSet(triple, "triple")
+        //printCardSet(tmp, "tmp")
         i = -1
         continue
       }
@@ -166,11 +202,10 @@ func attack() {
     // find double
     if i+2 <= len(tmp) {
       if tmp[i+1]/4 == tmp[i]/4 {
-        fmt.Printf("tmp[%d]: %d\n", i, tmp[i])
         double = append(double, tmp[i:i+2]...)
         tmp = append(tmp[:i], tmp[i+2:]...)
-        printCardSet(double, "double")
-        printCardSet(tmp, "tmp")
+        //printCardSet(double, "double")
+        //printCardSet(tmp, "tmp")
         i = -1
         continue
       }
@@ -181,4 +216,53 @@ func attack() {
   printCardSet(triple, "Triple")
   printCardSet(double, "Double")
   printCardSet(tmp, "Single")
+  
+  attack_card := []int{}
+  
+  for {
+    if (len(bomb) != 0) {
+      start_idx := len(bomb)-4
+      attack_card = append(attack_card, bomb[start_idx:]...)
+      break
+    }
+
+    if (len(triple) != 0) {
+      start_idx := len(triple)-3
+      attack_card = append(attack_card, triple[start_idx:]...)
+      break
+    }
+
+    if (len(double) != 0) {
+      start_idx := len(double)-2
+      attack_card = append(attack_card, double[start_idx:]...)
+      break
+    }
+
+    if (len(tmp) != 0) {
+      start_idx := len(tmp)-1
+      attack_card = append(attack_card, tmp[start_idx:]...)
+      break
+    }
+    break
+  }
+
+  printCardSet(attack_card, "Attack_card")
+  calculateDamage(attack_card)
+  
+  for j, val := range gHand {
+    if val == attack_card[0] {
+      gHand = append(gHand[:j], gHand[j+len(attack_card):]...)
+    }
+  }
+}
+
+func calculateDamage(attack_card []int) {
+  sum := 0
+  count := 0
+  for _, val := range attack_card {
+    sum += val
+    count++
+  }
+  fmt.Printf("\nDamage: %d\n", sum * count)
+  gDamage += sum * count
 }
